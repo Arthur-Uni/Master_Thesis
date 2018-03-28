cbe0b7d38ce8a9f2c0e27f6d0dcb5f290bf0304e
FPGA-PC\ruder <rbxryder@gmail.com>
2018-03-28 15:13:05 +0200

    set up quantization for chebyshev curves

---
+++ cheb_quantize.m
@@ -0,0 +1,22 @@
+function [ y ] = cheb_quantize( x, mode, wordlength, var)
+%PARAMETERS:
+%   x, vector, input
+%   mode, String, quantization mode ('float', 'fixed' ...)
+%   roundmode, String, round mode ('floor', 'ceil', ...)
+%   wordlength, int, input
+%   var, int, input
+%   y, vector, output, quantized output of input x
+
+    switch mode
+        case 1  %fixed point: var is # of fractional bits in this case
+     	  q_fi = quantizer('mode', 'fixed', 'roundmode', 'Round', 'overflowmode', 'saturate', 'format', [wordlength var]);
+        y = quantize(q_fi, x);
+     case 2  %floating point: var is length of exponent in this case
+           q_fl = quantizer('mode', 'float', 'roundmode', 'ceil', 'format', [word_length var]);
+            y = quantize(q_fl, x);
+        otherwise
+            y = x;
+    end
+
+end
+

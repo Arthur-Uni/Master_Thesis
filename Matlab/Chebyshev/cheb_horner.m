79e6569d60288e4acc888cbbbddbe37731a495f2
FPGA-PC\ruder <rbxryder@gmail.com>
2018-04-10 14:19:07 +0200

    calculate final chebyshev polynomial using horner scheme with the ability to test impact of quantization

---
+++ Matlab/Chebyshev/cheb_horner.m
@@ -0,0 +1,33 @@
+function [ y ] = cheb_horner( a, b, n, q_en, mode, wordlength, var)
+%  Parameters:
+%
+%    Input, real a, b, the domain of definition.
+%
+%    Input, integer n, the order of the polynomial
+%
+%    Input, boolean q_en, enable quantization
+%
+%    Input, integer mode, quantization mode: 1 = fixed point, 2 = floating point
+%
+%    Input, integer wordlength
+%
+%    Input, integer var
+%
+%    Output, vector y, final polynomial approximation
+
+dots = linspace(-1,1);
+
+c = cheb_poly_coeffs(a,b,n,q_en,mode,wordlength,var);
+
+k = length(dots);
+l = length(c);
+temp = c(1)*ones(1,k);
+
+for i=2:l
+    temp = temp.*dots + c(i);
+end
+
+y = temp;
+
+end
+

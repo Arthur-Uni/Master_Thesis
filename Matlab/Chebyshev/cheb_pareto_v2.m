2970a077ec64ada247b0daec980a6949e29128e0
FPGA-PC\ruder <rbxryder@gmail.com>
2018-05-02 15:04:23 +0200

    added function to calculate pareto plot for two inputs only

---
+++ Matlab/Chebyshev/cheb_pareto_v2.m
@@ -0,0 +1,20 @@
+function [ P ] = cheb_pareto_v2( M, N )
+
+k = length(M(:));
+
+M_temp = M;
+
+for i=1:k
+    M_bigger = M(i) >= M;
+    N_bigger = N(i) >= N;
+    temp = M_bigger .* N_bigger;
+    number_of_ones = nnz(temp);
+    if(number_of_ones > 1)
+        M_temp(i) = 0;
+    end
+end
+
+P = M_temp;
+
+end
+

f0b6273ed54b056cd76b97e4b8b65c7ecfb684ce
FPGA-PC\ruder <rbxryder@gmail.com>
2018-05-15 17:56:54 +0200

    fixed point part now works as intended for positive as well as negative part

--- Matlab/Shift/sig_PLA_1991.m
+++ Matlab/Shift/sig_PLA_1991.m
@@ -1,4 +1,4 @@
-function [ y ] = sig_PLA_1991( x )
+function [ y, y_fixed_point ] = sig_PLA_1991( x, wordlength )
 
 %% get all negative values of x
 neg_logical = x < 0;
@@ -33,20 +33,44 @@ end
 
 %% fixed point part
 
+fractionlength = wordlength - 1;
+
 %% negative part
 if(~isempty(x_neg))
     
+    % get integer part of x_neg
     x_neg_integer = abs(fix(x_neg));
     
-    y_neg_fractional = abs(x_neg + x_neg_integer);
-    y_neg_fractional = fi(y_neg_fractional, false, 32, 32);
+    % get fractional part of x_neg
+    x_neg_fractional = x_neg + x_neg_integer;
     
-    y_neg_fractional = bitshift(y_neg_fractional, -2);
+    n = zeros(1, size(x,2));
+    for i=1:size(x_neg_fractional,2);
+        if(x_neg_fractional(i) == 0 && x_neg_integer(i) ~= 0)
+            n(i) = x_neg_integer(i) - 1;
+        else
+            n(i) = x_neg_integer(i);
+        end
+    end
     
-    y_neg_fractional = bitset(y_neg_fractional, 31);
+    % convert to signed fixed point format
+    x_neg_fractional = fi(x_neg_fractional, true, wordlength, fractionlength);
     
-    for i=1:size(y_neg_fractional,2)
-        y_neg_fractional(i) = bitshift(y_neg_fractional(i), - x_neg_integer(i));
+    %% manipulate bit stream to directly convert to y_neg_fixed_point
+    
+    % remove sign bit
+    x_neg_fractional = bitset(x_neg_fractional, wordlength, 0);
+    
+    % shift two two bits to the right
+    x_neg_fractional = bitshift(x_neg_fractional, -2);
+    
+    % add 0.5 shifted one place to the right
+    x_neg_fractional = bitset(x_neg_fractional, wordlength - 2);
+    
+    %% shift x_neg_fractional to acquire y_neg_fixed_point
+    
+    for i=1:size(x_neg_fractional,2)
+        y_neg_fixed_point(i) = bitshift(x_neg_fractional(i), -n(i));
     end
     
 end
@@ -54,16 +78,37 @@ end
 %% positive part
 if(~isempty(x_pos))
     
-    x_pos_fractional = x_pos - abs(fix(x_pos));
+    x_pos_integer = abs(fix(x_pos));
     
-    x_pos_fractional = fi(x_pos_fractional, false, 32, 31);
+    x_pos_fractional = x_pos - x_pos_integer;
+    
+    x_pos_fractional = fi(x_pos_fractional, false, wordlength, wordlength);
     
     x_pos_fractional = bitshift(x_pos_fractional, -2);
     
-    x_pos_fractional = bitset(x_pos_fractional, 31);
+    x_pos_fractional = bitset(x_pos_fractional, 32);
     
-    x_pos_fractional = bitshift(x_pos_fractional, -abs(fix(x_pos)));
+    for i=1:size(x_pos_fractional,2)
+        y_pos_fixed_point(i) = bitshift(x_pos_fractional(i), -x_pos_integer(i));
+        k = wordlength;
+        for j=1:x_pos_integer(i)
+            y_pos_fixed_point(i) = bitset(y_pos_fixed_point(i), k);
+            k = k - 1;
+        end
+        
+    end
     
+%     y_pos_fixed_point = 1 - fliplr(y_neg_fixed_point);
+    
+end
+
+%% bring everything together for fixed point part
+if(isempty(x_neg))
+    y = y_pos_fixed_point;
+elseif(isempty(x_pos))
+    y = y_neg_fixed_point;
+else
+    y_fixed_point = [y_neg_fixed_point y_pos_fixed_point];
 end
 
 end

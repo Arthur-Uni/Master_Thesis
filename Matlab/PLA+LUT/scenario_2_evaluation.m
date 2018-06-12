c697e2044fca78f32d3d371e9407de66967f4397
FPGA-PC\ruder <rbxryder@gmail.com>
2018-06-12 15:37:11 +0200

    scenario 2 finished

---
+++ Matlab/PLA+LUT/scenario_2_evaluation.m
@@ -0,0 +1,43 @@
+%% inputs
+x_fractionlength = 12;
+x = -6:2^-x_fractionlength:6;
+
+NoE_1 = 4;
+NoE_2 = 8;
+NoE_3 = 1;
+
+[ y, LUT_Entries ] = sc2(x, NoE_1, NoE_2, NoE_3, x_fractionlength);
+
+%% error calculations
+t = tanh(x);
+abs_error = abs(t-y);
+rel_error = abs_error ./ t;
+mse = 1/numel(t) .* sum((t-y).^2);
+
+%% plot
+
+% parameters
+width = 2;
+fontSize = 14;
+
+figure(1)
+
+subplot(3,1,1);
+hold on;
+plot(x, t, 'LineWidth', width);
+plot(x, y, 'LineWidth', width);
+xlabel('x', 'FontSize', fontSize);
+ylabel('f(x)', 'FontSize', fontSize);
+set(gca, 'FontSize', fontSize);
+
+subplot(3,1,2);
+plot(x, abs_error, 'LineWidth', width);
+xlabel('x', 'FontSize', fontSize);
+ylabel('absolute error', 'FontSize', fontSize);
+set(gca, 'FontSize', fontSize);
+
+subplot(3,1,3);
+plot(x, rel_error, 'LineWidth', width);
+xlabel('x', 'FontSize', fontSize);
+ylabel('relative error', 'FontSize', fontSize);
+set(gca, 'FontSize', fontSize);

d58f1cad0afd8d4d356417698a6bebaeaf225a22
DESKTOP-B9222NP\Arthur <rbxryder@gmail.com>
2018-06-23 17:30:36 +0200

    simplified version of ELU

--- Matlab/Approximations/ELU.m
+++ Matlab/Approximations/ELU.m
@@ -1,14 +1,4 @@
 function [ y ] = ELU( x, alpha )
-
-y = zeros(1, size(x,2));
-
-%     for i = 1 : size(x,2)
-%         if(sign(x(i))== -1)
-%             y(i) = alpha * (exp(x(i))-1);
-%         else
-%             y(i) = x(i);
-%         end
-%     end
     
 y = max(0,x) + min(0,alpha.*(exp(x)-1));
 

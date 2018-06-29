4f312ef58d7a952b33507e93d84b2e7924a8ce6e
FPGA-PC\ruder <rbxryder@gmail.com>
2018-06-29 16:30:37 +0200

    modified bar script

--- Matlab/bar_plot_script.m
+++ Matlab/bar_plot_script.m
@@ -1,10 +1,18 @@
 % load('Results25052018.mat');
+close all;
+clc;
+
+set(0,'defaulttextinterpreter','latex');
+
+color = [0,0.3294118,0.623529];
 
 hold on;
-bar(Results);
-axis([0 length(Results)+1 min(Results)-0.001 max(Results)+0.001]);
+bar(Results, 'FaceColor', color);
+axis([0 length(Results)+1 min(Results)-0.005 max(Results)+0.005]);
 set(gca, 'FontSize', 16);
-text(1:length(Results),Results,num2str(Results'),'vert','bottom','horiz','center', 'FontSize', 16); 
+text(1:length(Results),Results + 0.001,num2str(Results'),'vert','bottom','horiz','center', 'FontSize', 16); 
 box off;
 ylabel('accuracy');
 xlabel('implementations');
+
+set(gca, 'FontSize', 16);

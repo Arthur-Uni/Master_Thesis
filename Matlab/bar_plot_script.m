9a48dcf31ffe2ecd24de4bac635a5c7ca12f6adf
FPGA-PC\ruder <rbxryder@gmail.com>
2018-07-24 15:36:44 +0200

    moved everything related to hyperbolic tangent in separate folder

--- Matlab/bar_plot_script.m
+++ Matlab/bar_plot_script.m
@@ -1,5 +1,5 @@
 % load('Results25052018.mat');
-close all;
+% close all;
 clc;
 
 set(0,'defaulttextinterpreter','latex');
@@ -8,11 +8,12 @@ color = [0,0.3294118,0.623529];
 
 hold on;
 bar(Results, 'FaceColor', color);
-axis([0 length(Results)+1 min(Results)-0.005 max(Results)+0.005]);
+axis([0.25 length(Results)+0.75 min(Results)-0.1 max(Results)+0.1]);
+%axis([0.25 length(Results)+0.75 0 30]);
 set(gca, 'FontSize', 16);
 text(1:length(Results),Results + 0.001,num2str(Results'),'vert','bottom','horiz','center', 'FontSize', 16); 
 box off;
-ylabel('accuracy');
-xlabel('implementations');
+%ylabel('accuracy');
+%xlabel('implementations');
 
 set(gca, 'FontSize', 16);
